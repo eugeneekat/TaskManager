34b1f77c5b2d8af4199d5eb12e8b76c85b2f8cfc
EugeneEkat <ekaterinenko88@gmail.com>
2016-11-30 14:18:50 +0200

    commit

--- TaskManager/MainWindow.xaml.cs
+++ TaskManager/MainWindow.xaml.cs
@@ -13,16 +13,73 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Management;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.ComponentModel;
+
+using System.Collections.ObjectModel;
+
 namespace TaskManager
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        ConcurrentDictionary<int, Process> dic = new ConcurrentDictionary<int, Process>(Process.GetProcesses().ToDictionary(x => x.Id));
+
+        SystemWatcher wathcer = new SystemWatcher();
+
         public MainWindow()
         {
             InitializeComponent();
+            //Process start/stop events subscribe  
+            wathcer.ProcessStart    += this.ProcessStart;
+            wathcer.ProcessStop     += this.ProcessStop;
+            this.ProcessListView.ItemsSource = dic.Values;
+        }
+
+        void ProcessStart(object sender, ProcessEventArgs e)
+        {
+            bool gettedProcess = true;
+            try
+            {
+                Process newProcess = Process.GetProcessById(e.ProcessID);
+                bool added = false;
+                while(!added)
+                    added = this.dic.TryAdd(newProcess.Id, newProcess);              
+            }
+            catch(Exception)
+            {
+                gettedProcess = false;
+            }
+            if (gettedProcess && !Dispatcher.CheckAccess())
+                Dispatcher.Invoke(this.UpdateProcesses);
+        }
+
+        void ProcessStop(object sender, ProcessEventArgs e)
+        {           
+            if(dic.ContainsKey(e.ProcessID))
+            {
+                Process proc = null;
+                while (proc == null)
+                    dic.TryRemove(e.ProcessID, out proc);
+            }           
+            if (!Dispatcher.CheckAccess())
+                Dispatcher.Invoke(this.UpdateProcesses);
+        }
+       
+        void UpdateProcesses()
+        {
+            this.ProcessListView.ItemsSource = dic.Values;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.wathcer.Dispose();
         }
     }
 }
---
+++ TaskManager/SystemWatcher.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.Windows;
+
+namespace TaskManager
+{
+    public class ProcessEventArgs
+    {
+        public int ProcessID { get; private set; }
+        public string ProcessName { get; private set; }
+        public ProcessEventArgs(int processID, string processName)
+        {
+            this.ProcessID = processID;
+            this.ProcessName = processName;
+        } 
+    }
+
+
+    class SystemWatcher : IDisposable
+    {
+        private bool disposed = false;
+
+        string machine = string.Format("\\\\{0}\\root\\cimv2", Environment.MachineName);
+
+        ManagementEventWatcher processStartWathcher = null;
+        ManagementEventWatcher processStopWathcher = null;
+        //High level - eventhandler delegate type
+        public delegate void ProcessEventHandler (object sender, ProcessEventArgs e);
+        //High level - process start/stop events
+        public event ProcessEventHandler ProcessStart = null;
+        public event ProcessEventHandler ProcessStop = null;
+
+        public SystemWatcher()
+        {
+            this.processStartWathcher = new ManagementEventWatcher(machine, "SELECT * FROM Win32_ProcessStartTrace");
+            this.processStopWathcher = new ManagementEventWatcher(machine, "SELECT * FROM Win32_ProcessStopTrace");
+            processStartWathcher.EventArrived += new EventArrivedEventHandler(processStartEvent_EventArrived);
+            processStopWathcher.EventArrived += new EventArrivedEventHandler(processStopEvent_EventArrived);
+            processStartWathcher.Start();
+            processStopWathcher.Start();
+        }
+
+       
+
+        //Low level process start/stop event handlers
+        void processStartEvent_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            //Get processID and pass to High level subscribers
+            int processID = Convert.ToInt32(e.NewEvent.Properties["ProcessID"].Value);
+            string processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            ProcessStart?.Invoke(this, new ProcessEventArgs(processID, processName));
+        }
+        void processStopEvent_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            int processID = Convert.ToInt32(e.NewEvent.Properties["ProcessID"].Value);
+            string processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            ProcessStop?.Invoke(this, new ProcessEventArgs(processID, processName));
+        }
+
+        /*-------------------Dispose--------------------*/
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!disposed)
+            {
+                if(disposing)
+                {
+                    this.processStartWathcher.Dispose();
+                    this.processStopWathcher.Dispose();
+                }
+                disposed = true;
+            }
+        }
+        ~SystemWatcher()
+        {
+            this.Dispose(false);
+        }
+    }
+}
